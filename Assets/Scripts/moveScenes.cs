34dc366e012ec94441dd18c68e7015d731df6c0d
maryam lootah <mkl8710@nyu.edu>
2026-09-24 11:55:12 +0400

    game is playable

--- Assets/Scripts/moveScenes.cs
+++ Assets/Scripts/moveScenes.cs
@@ -4,13 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class moveScenes : MonoBehaviour
 {
-    public bool changeScene = false;
     private BoxCollider2D boxCollider2D;
     public string sceneName = "";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         boxCollider2D = GetComponent<BoxCollider2D>();
+        if (boxCollider2D != null)
+        {
+            boxCollider2D.isTrigger = true;
+        }
     }
 
     // Update is called once per frame
@@ -19,14 +22,11 @@ public class moveScenes : MonoBehaviour
         
     }
 
-    public void onTriggerEnter2d(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (changeScene)
+        if (!string.IsNullOrEmpty(sceneName) && other.CompareTag("Player"))
         {
-            if (other.tag == "player")
-            {
-                LoadScene(sceneName);
-            }
+            LoadScene(sceneName);
         }
     }
 
